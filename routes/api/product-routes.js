8500d1fc5527d080f378e2d2a2e785ee264b1648
Skruphold <hunterco999@gmail.com>
2022-01-31 17:10:46 -0600

    added the rest of the routes everything works fine one error but doesnt seem to stop it from still working. to work on this next

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,20 +19,27 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  try {
-    const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category, through: ProductTag, as: 'product_id' }]
-    });
-
-    if (!productData) {
-      res.status(404).json({ message: 'No product found with this id!' });
-      return;
+  Product.findOne(
+    {
+      where: {
+        id: req.params.id
+      },
+      include: [{
+        model: Category,
+        attributes: ['category_name']
+      },
+    {
+      model: Tag,
+      attributes: ['tag_name'],
+      as: 'product_id'
+    }]
     }
-
-    res.status(200).json(productData);
-  } catch (err) {
+  )
+  .then(productData => res.json(productData))
+  .catch(err => {
+    console.log(err);
     res.status(500).json(err);
-  }
+  })
 });
 
 // create new product
